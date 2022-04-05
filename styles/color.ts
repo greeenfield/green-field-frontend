422dac10f3550b6eb68a8543002ee1245d9bef1e
curlyjun <tjdwns4850@gmail.com>
2022-04-05 23:20:17 +0900

    featL Update globalCss

--- styles/color.ts
+++ styles/color.ts
@@ -1,1 +1,4 @@
 export class Color {}
+export const COLOR = {
+  BLACK: "#0B0F0C",
+}
--- styles/globals.tsx
+++ styles/globals.tsx
@@ -1,12 +1,26 @@
 import { Global, css } from "@emotion/react"
+import { COLOR } from "./color"
 
 export const globalStyles = (
   <Global
     styles={css`
       html,
       body {
+        padding: 0;
         margin: 0;
         height: 100%;
+        color: ${COLOR.BLACK};
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+
+      * {
+        box-sizing: border-box;
       }
     `}
   />
