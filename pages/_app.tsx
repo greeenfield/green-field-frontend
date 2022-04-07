1bee72c8b6f43c7c9749f5a1e4b0cffee58cc4fe
curlyjun <tjdwns4850@gmail.com>
2022-04-07 17:47:38 +0900

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import { globalStyles } from "styles/globals"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>그린 필드</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       {globalStyles}
       <Component {...pageProps} />
     </>
