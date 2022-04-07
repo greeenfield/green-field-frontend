66bd2e24e80d7c5dd3808d80c0a11e1255b81c09
curlyjun <tjdwns4850@gmail.com>
2022-04-07 19:12:59 +0900

    env: svgr 추가, prettier 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: { plugins: [{ name: 'removeViewBox', active: false }] },
+          },
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
