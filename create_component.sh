4eeb96511be7e71aa3011601d859d3ab31888d12
curlyjun <tjdwns4850@gmail.com>
2022-05-27 02:38:23 +0900

    env: 컴포넌트 자동 생성 쉘 스크립트 추가

---
+++ create_component.sh
@@ -0,0 +1,17 @@
+
+
+# 입력 받기
+echo "폴더명 입력"
+read folder
+echo "컴포넌트명 입력"
+read component
+
+set -e
+
+
+mkdir src/components/$folder
+echo "import { S$component } from './Footer.style';\n\ninterface ${component}Props {}\n\nconst $component = ({}: ${component}Props) => {\n  return <S$component></S$component>;\n};\n\nexport default $component;" > src/components/$folder/$component.tsx
+echo "export { default as $component } from './$component';" > src/components/$folder/index.ts
+echo "import styled from '@emotion/styled';\n\nexport const S$component = styled.div\`\`;" > src/components/$folder/$component.style.ts
+
+
