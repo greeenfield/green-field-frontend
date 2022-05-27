2ab1537db0a52368f2fdeb5715b45c3ea50a19b8
curlyjun <tjdwns4850@gmail.com>
2022-05-27 17:08:23 +0900

    fix: Fix typo

--- create_component.sh
+++ create_component.sh
@@ -10,7 +10,7 @@ set -e
 
 
 mkdir src/components/$folder
-echo "import { S$component } from './$comonent.style';\n\ninterface ${component}Props {}\n\nconst $component = ({}: ${component}Props) => {\n  return <S$component></S$component>;\n};\n\nexport default $component;" > src/components/$folder/$component.tsx
+echo "import { S$component } from './$component.style';\n\ninterface ${component}Props {}\n\nconst $component = ({}: ${component}Props) => {\n  return <S$component></S$component>;\n};\n\nexport default $component;" > src/components/$folder/$component.tsx
 echo "export { default as $component } from './$component';" > src/components/$folder/index.ts
 echo "import styled from '@emotion/styled';\n\nexport const S$component = styled.div\`\`;" > src/components/$folder/$component.style.ts
 
