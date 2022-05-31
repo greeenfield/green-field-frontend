28ac13bd3cc3f22e268ed27bdc38571b53060b58
curlyjun <tjdwns4850@gmail.com>
2022-05-31 16:23:53 +0900

    env: Add axios, react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,30 @@
 import { Layout } from '@components/layout';
 import { globalStyles } from '@styles/globals';
+import axios from 'axios';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+
+axios.defaults.baseURL = 'http://localhost:3001/';
+axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <title>그린 필드</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {globalStyles}
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
