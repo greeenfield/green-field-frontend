df652396510269dfcb26dcb6e9f67bd19216d384
curlyjun <tjdwns4850@gmail.com>
2022-06-09 13:41:48 +0900

    feat: getMe api 추가

---
+++ src/apis/user.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+import { User } from '@models/user';
+
+export const getMe = async (cookie?: string) => {
+  try {
+    if (cookie) {
+      // SSR
+      const { data } = await axios.get<User>('/user/me', {
+        headers: {
+          Cookie: cookie,
+        },
+      });
+
+      return data;
+    } else {
+      // CSR
+      const { data } = await axios.get<User>('/user/me');
+      return data;
+    }
+  } catch (error) {
+    return null;
+  }
+};
