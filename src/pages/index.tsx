dc05113164d1efc5227a08b787b96f21708d5d3d
curlyjun <tjdwns4850@gmail.com>
2022-06-10 21:45:25 +0900

    feat: main note 가져오는 api 로직 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,37 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 
-const Home: NextPage = () => {
-  return <div>메인 dkssud</div>;
+import * as noteApi from '@apis/note';
+import { MainNote } from '@components/mainNote';
+import styled from '@emotion/styled';
+import { Note } from '@models/note';
+
+interface HomeProps {
+  notes: Note[];
+}
+
+const Home: NextPage<HomeProps> = ({ notes }) => {
+  return (
+    <HomeWrapper>
+      {notes.map((note) => (
+        <MainNote key={note.id} note={note} />
+      ))}
+    </HomeWrapper>
+  );
 };
 
+const HomeWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
 export default Home;
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const { data } = await noteApi.get();
+  return {
+    props: {
+      notes: data,
+    },
+  };
+};
