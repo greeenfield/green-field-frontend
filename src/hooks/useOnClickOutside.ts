7552cf4f3081ca6fb8b6899c19a7ed574dbed42d
curlyjun <tjdwns4850@gmail.com>
2022-06-21 17:29:20 +0900

    feat: useOnclickOutside hook 추가

---
+++ src/hooks/useOnClickOutside.ts
@@ -0,0 +1,21 @@
+import { EventHandler, RefObject, useEffect } from 'react';
+
+export const useOnClickOutside = (
+  ref: RefObject<HTMLElement>,
+  handler: (e: EventHandler) => void
+) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      if (ref && (!ref.current || ref.current.contains(event.target))) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]);
+};
