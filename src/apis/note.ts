94fecb6a9030ebb8637a6cf5002eacef2e444ba0
curlyjun <tjdwns4850@gmail.com>
2022-07-05 18:54:42 +0900

    feat: uploadThumbnail api 추가

--- src/apis/note.ts
+++ src/apis/note.ts
@@ -3,3 +3,11 @@ import axios from 'axios';
 export const get = () => {
   return axios.get('/note');
 };
+
+export const uploadThumbnail = async (formData: FormData) => {
+  const { data } = await axios.post<{ location: string }>(
+    '/note/upload',
+    formData
+  );
+  return data;
+};
